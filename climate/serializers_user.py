776c3717d74a0f76d5840f9570178fd7eaa6f886
Maxence Cerisier <maxence.cerisier56@gmail.com>
2023-07-05 11:42:44 +0200

    login qui fonctionne !

--- climate/serializers_user.py
+++ climate/serializers_user.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'password')
+        fields = ('first_name', 'last_name', 'username', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
